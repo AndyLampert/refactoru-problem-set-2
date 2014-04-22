523a555ba0e78ac1f8e83066ea149ee1f45afbc1
AndyLampert <lamperta@gmail.com>
2014-04-21 19:22:56 -0600

    fixed jasmine link

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 
 // var swapCase = function(str) {
 // 	var newString = str.split('');
-// 	var newArray = [];
+// 	var newArray = [];ls
 // 	for(var i=0;i<newString.length;i++){
 // 		// if the array value.toLowerCase  is ALREADY lowercase (ie. if when I change it to lowercase, it's the same as original)
 // 		// put that value after transforming it to uppercase 
@@ -43,10 +43,6 @@ console.log(swapCase("Hello World"));
 var letterCount = function() {
 	
 }
-// #2
-// Write a function swapCase that takes a single string parameter and swaps the case of each character. 
-// For example: if a string is "Hello World" the output should be "hELLO wORLD". Let numbers and symbols stay the way they are.
-
 // Bonus:
 // Write a function letterCount that takes a single string parameter and returns the first word with the greatest number of repeated letters. For example: "Today, is the greatest day ever!" should return greatest because it has 2 e's (and 2 t's) and it comes before ever which also has 2 e's. If there are no words with repeating letters return -1. Words will be separated by single spaces.
 
